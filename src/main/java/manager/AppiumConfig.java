31624e26d2d9d8c7c0d5d8ffba38e0876ed84ab8
alexkhalifa <alexkhalifamusic@gmail.com>
2026-03-20 12:02:13 +0200

    Start Test

---
+++ src/main/java/manager/AppiumConfig.java
@@ -0,0 +1,28 @@
+package manager;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static utils.PropertiesReader.*;
+
+public class AppiumConfig {
+    public static AppiumDriver createAppiumDriver(String fileName){
+        UiAutomator2Options options = new UiAutomator2Options()
+                .setPlatformName(getProperty(fileName, "os"))
+                .setAutomationName(getProperty(fileName, "automationName"))
+                .setDeviceName(getProperty(fileName, "deviceName"))
+                .setAppPackage(getProperty(fileName, "appPackage"))
+                .setAppActivity(getProperty(fileName, "appActivity"));
+        try {
+            AppiumDriver appiumDriver = new AppiumDriver
+                    (new URL(getProperty(fileName, "appiumUrl")), options);
+            return appiumDriver;
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Bad Appium URL: " +
+                    getProperty(fileName, "appiumURL"), e);
+        }
+    }
+}
